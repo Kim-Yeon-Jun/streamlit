93cef62b8705ff6a2f985b950c4d5b7ed81f4fa8
Kim-Yeon-Jun <106429088+Kim-Yeon-Jun@users.noreply.github.com>
2023-10-24 15:41:19 +0900

    add button

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ st.code(code)
 
 st.markdown('스트림릿에서 **마크다운**을 사용할 수 있습니다.:sunglasses:')
 
+st.title("스트림릿의 버튼 입력 사용 예")
+
+clicked = st.button('버튼1')
+st.write('버튼 1 클릭 상태:', clicked)
+
+if clicked:
+    st.write('버튼 1을 클릭했습니다.')
+else:
+    st.write('버튼 1을 클릭하지 않았습니다.')
+
